c17f1ef444b4cae8a1b6955c5b6d1c0cf23bc814
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-10-03 15:28:35 +0000

    1.2

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-import json, time, re
+import json, time, re, copy
 from flask import Flask, redirect, url_for, request,render_template, send_file, send_from_directory
+from code_utils.general import *
 from code_utils.backend_utils import *
+from code_utils.rnn_utils import *
+from code_utils.cat_utils import *
+from code_utils.sqld_utils import *
 
 app = Flask(__name__)
 
 upload_dir="uploaded"
 if not os.path.exists(upload_dir): os.makedirs(upload_dir)
+
+nn_model_path="data-models/model-refined-full-20wv-1-408-1-128-1-0.001-50000.model"
+loaded_model=load_nn(nn_model_path,seq_nn_OLD,extract_ft_lb,epoch_i=5)
+
+refined_first_token_dict_fpath="data-models/refined_first_token_dict.sqld"
+refined_first_token_dict=open_sqld(refined_first_token_dict_fpath)
+
+no_context_first_token_dict_fpath="data-models/no_context_first_token_dict.sqld"
+no_context_first_token_dict=open_sqld(no_context_first_token_dict_fpath)
 
 
 #app = Flask(__name__, static_url_path='/assets')
@@ -35,6 +48,7 @@ def process():
         #posted_data=posted_data.decode("utf-8")
         #cur_dict={"request_type":"POST"}
         #request.data
+
         cur_dict["data"]=posted_data_dict
         cur_dict["time"]=time.ctime()
         fopen=open("query_log.txt","a")
@@ -58,10 +72,13 @@ def upload():
         uploaded_fpath=os.path.join(upload_dir,file_name)
         write_base64(file_content,uploaded_fpath)
         posted_data_dict.pop('uploaded_file', None)
+        cur_dict=copy.deepcopy(posted_data_dict)
 
         cur_dict["fpath"]=uploaded_fpath
-        cur_dict["data"]=posted_data_dict
+        #cur_dict["data"]=posted_data_dict
         cur_dict["time"]=time.ctime()
+        cur_dict["action"]="upload"
+        cur_dict["success"]=True
         fopen=open("query_log.txt","a")
         fopen.write(json.dumps(cur_dict)+"\n")
         fopen.close()
