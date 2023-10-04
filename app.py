e910a0b146b2283949b9a40d6884fbade1e48cb2
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-10-04 15:53:31 +0000

    1.003

--- app.py
+++ app.py
@@ -17,8 +17,12 @@ if not os.path.exists(output_dir): os.makedirs(output_dir)
 
 wv_fpath0="data-models/editing_word2vec_1198062_20.model"
 
-nn_model_path="data-models/model-refined-full-20wv-1-408-1-128-1-0.001-50000.model"
-loaded_model=load_nn(nn_model_path,seq_nn_OLD,extract_ft_lb,epoch_i=5,cur_wv_path=wv_fpath0)
+# nn_model_path="data-models/model-refined-full-20wv-1-408-1-128-1-0.001-50000.model"
+# loaded_model_OLD=load_nn(nn_model_path,seq_nn_OLD,extract_ft_lb,epoch_i=5,cur_wv_path=wv_fpath0)
+
+nn_model_path2="model-refined-nn-iter-full-new-1-408-1-256-1-0.0001-10000.model"
+loaded_model=load_nn(nn_model_path2,seq_nn,extract_ft_lb,epoch_i=None,cur_wv_path=wv_fpath0)
+
 
 refined_first_token_dict_fpath="data-models/refined_first_token_dict.sqld"
 refined_first_token_dict=open_sqld(refined_first_token_dict_fpath)
@@ -54,7 +58,38 @@ def check_repl():
     # except: output=json.dumps(cur_dict) 
     return json.dumps(cur_dict) 
 
+def pre_edit(sent_str,nn_model_obj,first_token_dict,pred_threshold=0.5): #pre-edit a sentence giveb the first token dict and a neural network model object with prediction
+  sent_tokens0=general.add_padding(general.tok(sent_str))
+  span_repl_items=extract_repl_instances(sent_tokens0,[],first_token_dict)
+  new_items=[]
+  for item0 in span_repl_items:
+    if item0["context"]=='|': continue
+    #if nn_model_obj==None: wt0=0
+    wt0=nn_model_obj.pred(item0)
+    # try: wt0=nn_model_obj.pred(item0)
+    # except: wt0=0
+    #print(item0,wt0)
+    #if wt0<pred_threshold: continue
+    item0["wt"]=wt0
+    new_items.append(item0)
+  if nn_model_obj!=None: pre_edited_sent_tokens,valid_repl=repl_span_phrase(sent_tokens0,new_items,sort_by="wt",min_wt=pred_threshold)
+  else: pre_edited_sent_tokens,valid_repl=repl_span_phrase(sent_tokens0,new_items,sort_by="freq",min_wt=0)
+  if pre_edited_sent_tokens[0]=="<s>": pre_edited_sent_tokens=pre_edited_sent_tokens[1:]
+  if pre_edited_sent_tokens[-1]=="</s>": pre_edited_sent_tokens=pre_edited_sent_tokens[:-1]
+  return pre_edited_sent_tokens,valid_repl
 
+#pre_edit(sent_str,nn_model_obj,first_token_dict,pred_threshold=0.5)
+@app.route('/pre_edit_api',methods = ['POST', 'GET'])
+def pre_edit_api():
+    cur_dict={}
+    
+    sent="UN missions in DRC and CAR, Chad and Sudan"
+    cur_dict["sent"]=sent
+    cur_dict["dir"]=dir(loaded_model)
+    # pre_edited_sent_tokens,valid_repl=pre_edit(sent,loaded_model,refined_first_token_dict,pred_threshold=0)
+    # cur_dict["tokens"]=pre_edited_sent_tokens
+    # cur_dict["repl_list"]=valid_repl
+    return json.dumps(cur_dict)
 
 @app.route('/interface',methods = ['POST', 'GET'])
 def interface():
@@ -80,7 +115,8 @@ def process():
         
         cur_docx_path=posted_data_dict.get("fpath")
         #new_edit_pre_edit_list,all_repl_inst_list,analysis_dict=analyze_pre_edit_docx(cur_docx_path,loaded_model=None,refined_first_token_dict,pred_threshold=0.3)
-        new_edit_pre_edit_list,all_repl_inst_list,analysis_dict=analyze_pre_edit_docx(cur_docx_path,None,no_context_first_token_dict,pred_threshold=0.3)
+        #new_edit_pre_edit_list,all_repl_inst_list,analysis_dict=analyze_pre_edit_docx(cur_docx_path,None,no_context_first_token_dict,pred_threshold=0.3)
+        new_edit_pre_edit_list,all_repl_inst_list,analysis_dict=analyze_pre_edit_docx(cur_docx_path,loaded_model,refined_first_token_dict,pred_threshold=0.3)
         new_edit_list=[(v[1],v[4],v[-1]) for v in new_edit_pre_edit_list]
         cur_docx_fname=os.path.split(cur_docx_path)[-1]
         cur_html_fname=cur_docx_fname.replace(".docx",".html")
@@ -90,7 +126,6 @@ def process():
         edit_list2html(new_edit_list,cur_html_fpath,template_fpath=cur_template_fpath)
         cur_dict=copy.deepcopy(posted_data_dict)
         cur_dict["cur_html_fpath"]=cur_html_fpath
-
         #posted_data=posted_data.decode("utf-8")
         #cur_dict={"request_type":"POST"}
         #request.data
